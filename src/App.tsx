6a5b1aecc9b365e51ddeaafb954a2897fc8f9460
Brian Gates <briandotgates@gmail.com>
2021-01-18 14:39:12 -0500

    get slider to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import React, { useState, useRef } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  GeoJSON,
+  MapConsumer,
+} from "react-leaflet";
+import { geoJSON, GeoJSON as LeafletGeoJSON, Map as LeafletMap } from "leaflet";
 import "./App.css";
+import DateSlider from "./DateSlider";
 import { statesData } from "./state-border-geojson";
 
 const colors = [
@@ -32,34 +41,41 @@ const getColor = (n: number) =>
     ? colors[2]
     : colors[1];
 
-const getStyle = (feature?: GeoJSON.Feature) => ({
-        fillColor: getColor(feature?.properties?.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-})
-
 function App() {
+  const [date, setDate] = useState<number>(1790);
+
+  const getStyle = (feature?: GeoJSON.Feature) => ({
+    fillColor: getColor(feature?.properties?.density),
+    weight: 2,
+    opacity: 1,
+    color: "white",
+    dashArray: "3",
+    fillOpacity: 0.7,
+  });
+  const handleChange = (map: LeafletMap) => (d: number) => {
+    console.log({ d, map });
+    setDate(d);
+    const data = {
+      ...statesData,
+      features: statesData.features.filter(
+        (feature) =>
+          feature.properties.admitted && feature.properties.admitted <= d
+      ),
+    };
+    map.eachLayer((layer) => {
+      if (layer.getAttribution?.() === "US Census") {
+        map.removeLayer(layer);
+      }
+    });
+    geoJSON(data, { style: getStyle, attribution: "US Census" }).addTo(map);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <MapContainer center={[37.0, -96.5]} zoom={5} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-          <GeoJSON data={statesData} style={getStyle} />
-        </MapContainer>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -69,6 +85,40 @@ function App() {
           Learn React
         </a>
       </header>
+      <MapContainer center={[37.0, -96.5]} zoom={5} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <GeoJSON data={statesData} style={getStyle} attribution="US Census" />
+        <MapConsumer>
+          {(map) => (
+            <div
+              style={{
+                padding: 10,
+                position: "absolute",
+                bottom: "-10vh",
+                width: "100%",
+                background: "black",
+              }}
+              onMouseOver={() => map.dragging.disable()}
+              onMouseOut={() => map.dragging.enable()}
+            >
+              <DateSlider
+                min={1790}
+                max={2010}
+                value={date}
+                onChange={handleChange(map)}
+              />
+            </div>
+          )}
+        </MapConsumer>
+      </MapContainer>
     </div>
   );
 }
---
+++ src/DateSlider.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import "rc-slider/assets/index.css";
+import Slider from "rc-slider";
+
+// https://codesandbox.io/s/w0w6xyon2w
+type DateSliderProps = {
+  value: number;
+  min: number;
+  max: number;
+  onChange: (date: number) => void;
+};
+const DateSlider = ({ value, min, max, onChange }: DateSliderProps) => {
+  return (
+    <Slider
+      style={{ margin: "1rem", zIndex: 100000 }}
+      min={min}
+      max={max}
+      step={10}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+export default DateSlider;
