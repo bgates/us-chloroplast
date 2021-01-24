aa3ca287ab3d00f215c5fda98fa440b6ccca945f
Brian Gates <briandotgates@gmail.com>
2021-01-24 18:33:59 -0500

    change tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        blue: {
+          "map-100": "var(--colors-blue-100)",
+          "map-200": "var(--colors-blue-200)",
+          "map-300": "var(--colors-blue-300)",
+          "map-400": "var(--colors-blue-400)",
+          "map-500": "var(--colors-blue-500)",
+          "map-600": "var(--colors-blue-600)",
+          "map-700": "var(--colors-blue-700)",
+          "map-800": "var(--colors-blue-800)",
+          "map-900": "var(--colors-blue-900)",
+        },
+      },
       zIndex: {
         1000: 1000,
       },
